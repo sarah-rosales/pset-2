3ef5492ed9e8f90ebed83b96ad0f1d8aae7a6512
Sarah Rosales <srosales@ucvts.org>
2019-10-24 12:22:17 -0400

    exercise 5

---
+++ src/diagonal.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+
+const width = readlineSync.question("\nWidth: ");
+const length = readlineSync.question("Length: ");
+let diagonal = Math.hypot(width, length)
+
+diagonal = diagonal.toLocaleString("en", {maximumFractionDigits: 2, minimumFractionDigits: 2});
+
+console.log("\nA(n) " + width + "-by-" + length + "-inch sheet of paper has a diagonal of " + diagonal + " inch(s).");
