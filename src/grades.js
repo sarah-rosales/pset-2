301f5f647587d34763c46eacf776766099183ff9
Sarah Rosales <srosales@ucvts.org>
2019-10-27 18:53:53 -0400

    exercise 6

--- src/grades.js
+++ src/grades.js
@@ -12,9 +12,9 @@ const first_test = readlineSync.question("\nEnter three test grades.\n");
 const second_test = readlineSync.question("");
 const third_test = readlineSync.question("");
 
-let homework_weight = (0.15*first_homework + 0.15*second_homework + 0.15*third_homework)/3;
-let quiz_weight = (0.35*first_quiz + 0.35*second_quiz + 0.35*third_quiz)/3;
-let test_weight = (0.5*first_test + 0.5*second_test + 0.5*third_test)/3;
+const homework_weight = (0.15*first_homework + 0.15*second_homework + 0.15*third_homework)/3;
+const quiz_weight = (0.35*first_quiz + 0.35*second_quiz + 0.35*third_quiz)/3;
+const test_weight = (0.5*first_test + 0.5*second_test + 0.5*third_test)/3;
 
 let mp_grade = homework_weight + quiz_weight + test_weight;
 
