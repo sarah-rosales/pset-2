f6b65ae476a6400c6f699496d773118c5085bad1
Sarah Rosales <srosales@ucvts.org>
2019-10-27 18:54:16 -0400

    exercise 8

--- src/salary.js
+++ src/salary.js
@@ -2,14 +2,14 @@ const readlineSync = require("readline-sync");
 
 const annual = readlineSync.question("\nAnnual salary: ");
 
-let month = annual / 24
+const month = annual / 24
 
-let pre_tax = (month * 0.93);
+const pre_tax = (month * 0.93);
 
-let fit = (pre_tax * 0.157);
-let sit = (pre_tax * 0.0447);
-let sst = (pre_tax * 0.062);
-let mt = (pre_tax * 0.0145);
+const fit = (pre_tax * 0.157);
+const sit = (pre_tax * 0.0447);
+const sst = (pre_tax * 0.062);
+const mt = (pre_tax * 0.0145);
 
 let salary = pre_tax - (fit + sit + sst + mt);
 
